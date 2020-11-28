8f6923f386f280c15f7b1ebf8b048aac216a38ef
Robert Ripoll López <robriplopez@gmail.com>
2020-11-29 00:45:05 +0100

    App

--- frontend/views/site/mobile-app.php
+++ frontend/views/site/mobile-app.php
@@ -1,17 +1,22 @@
+<?php
+
+$this->title = 'Aplicación móvil';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header border-0">
+            <div class="card card-primary">
+                <div class="card-header">
                     <div class="d-flex justify-content-between">
-                        <h3 class="card-title">COVID Radar per a Google PLay</h3>
+                        <h3 class="card-title"><i class="fab fa-android"></i>&nbsp;&nbsp;COVID Radar para Android</h3>
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="card-body text-center">
                     <div class="position-relative mb-4">
-                        <a href="https://play.google.com/store/apps/details?id=es.gob.radarcovid&hl=es&gl=US" target="_blank">
+                        <a href="https://play.google.com/store/apps/details?id=es.gob.radarcovid" target="_blank">
                             <img src="/images/get_it_google_play.png" style="height: 100px">
-                            <span style="color: black">Click para descargar la aplicación para Google play</span>
                         </a>
                     </div>
                 </div>
@@ -19,17 +24,16 @@
             <!-- /.card -->
         </div>
         <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header border-0">
+            <div class="card card-primary">
+                <div class="card-header">
                     <div class="d-flex justify-content-between">
-                        <h3 class="card-title">COVID Radar per a Apple Store</h3>
+                        <h3 class="card-title"><i class="fab fa-apple"></i>&nbsp;&nbsp;COVID Radar para Apple</h3>
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="card-body text-center">
                     <div class="position-relative mb-4">
                         <a href="https://apps.apple.com/es/app/radar-covid/id1520443509" target="_blank">
                             <img src="/images/get_it_apple_store.png" style="height: 100px">
-                            <span style="color: black">Click para descargar la aplicación para Apple Store</span>
                         </a>
                     </div>
                 </div>
