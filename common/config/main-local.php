5087f692c4dc7df43ffd8cd49383e84ec04aa82f
Robert Ripoll López <robriplopez@gmail.com>
2020-11-28 18:29:00 +0100

    Fix config

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -19,13 +19,7 @@ if ($databaseUrl)
 
 return [
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=' . parse_url($databaseUrl, PHP_URL_HOST) . ';port=' . parse_url($databaseUrl, PHP_URL_PORT) . ';dbname=' . parse_url($databaseUrl, PHP_URL_PATH),
-            'username' => parse_url($databaseUrl, PHP_URL_USER),
-            'password' => parse_url($databaseUrl, PHP_URL_PASS),
-            'charset' => 'utf8',
-        ],
+        'db' => $db,
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
