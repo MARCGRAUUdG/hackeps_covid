87349aa9f451323bbef05582cf61611577dae855
Robert Ripoll López <robriplopez@gmail.com>
2020-11-29 04:31:01 +0100

    Fixes.

--- frontend/views/layouts/sidebar.php
+++ frontend/views/layouts/sidebar.php
@@ -23,14 +23,7 @@ $role = $authed ? (int)Yii::$app->user->identity->role : null;
             echo \hail812\adminlte3\widgets\Menu::widget([
                 'items' => [
                     ['label' => 'Estadísticas', 'url' => ['/'], 'icon' => 'tachometer-alt'],
-                    [
-                        'label' => 'Pruebas',
-                        'icon' => 'syringe',
-                        'items' => [
-                            ['label' => 'Centros', 'url' => ['/pruebas/centros'], 'icon' => 'clinic-medical', 'iconStyle' => 'far fas'],
-                            ['label' => 'Mis pruebas', 'url' => ['/pruebas/mis'], 'icon' => 'notes-medical', 'iconStyle' => 'far fas', 'visible' => $role === User::ROLE_USER],
-                        ]
-                    ],
+                    ['label' => 'Centros', 'url' => ['/centros'], 'icon' => 'clinic-medical'],
                     ['label' => 'Consultas', 'url' => ['/consultas'], 'icon' => 'user-md', 'visible' => $authed],
                     ['label' => 'Preguntas Frecuentes', 'url' => ['/faq'], 'icon' => 'question-circle'],
                     ['label' => 'Contacto', 'url' => ['/contacto'], 'icon' => 'envelope'],
@@ -40,36 +33,12 @@ $role = $authed ? (int)Yii::$app->user->identity->role : null;
                         'icon' => 'cogs',
                         'visible' => $role === User::ROLE_ADMIN,
                         'items' => [
-                            ['label' => 'Estadísticas', 'url' => ['/admin/estadisticas'], 'icon' => 'tachometer-alt', 'iconStyle' => 'far fas'],
-                            ['label' => 'Usuarios', 'url' => ['/admin/usuarios'], 'icon' => 'user', 'iconStyle' => 'far fas'],
                             ['label' => 'Consultas', 'url' => ['/admin/consultas'], 'icon' => 'user-md', 'iconStyle' => 'far fas'],
                             ['label' => 'Centros', 'url' => ['/admin/centros'], 'icon' => 'clinic-medical', 'iconStyle' => 'far fas'],
                             ['label' => 'Preguntas Frecuentes', 'url' => ['/admin/faq'], 'icon' => 'question-circle', 'iconStyle' => 'far fas'],
                             ['label' => 'Contacto', 'url' => ['/admin/contacto'], 'icon' => 'envelope', 'iconStyle' => 'far fas'],
                         ]
                     ],
-
-
-                    /*['label' => 'Yii2 PROVIDED', 'header' => true],
-                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],*/
-                    /*['label' => 'Level1'],
-                    [
-                        'label' => 'Level1',
-                        'items' => [
-                            ['label' => 'Level2', 'iconStyle' => 'far'],
-                            [
-                                'label' => 'Level2',
-                                'iconStyle' => 'far',
-                                'items' => [
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
-                                ]
-                            ],
-                            ['label' => 'Level2', 'iconStyle' => 'far']
-                        ]
-                    ],*/
                 ],
             ]);
             ?>
